f348cfd94c0b3943b0c409c9c4b96d064fe82025
ИванМензульский <vanyamenzulskiy@gmail.com>
2026-05-17 02:46:40 +0300

    Update forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -1,19 +1,13 @@
 from django import forms
 
-from .constants import DESCRIPTION_TEXTAREA_ROWS
-from .models import Project
+from projects.constants import DESCRIPTION_TEXTAREA_ROWS
+from projects.models import Project
 
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ["name", "description", "github_url", "status"]
-        labels = {
-            "name": "Название проекта",
-            "description": "Описание проекта",
-            "github_url": "Ссылка на Github",
-            "status": "Статус",
-        }
         widgets = {
             "description": forms.Textarea(
                 attrs={"rows": DESCRIPTION_TEXTAREA_ROWS},
